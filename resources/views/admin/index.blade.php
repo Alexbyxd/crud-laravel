6bb5c1d67d1bd2d1234a44a5ca39c9ab63008ea5
Alex Baltazar <alexbaltazarcastellon141@gmail.com>
2024-04-30 20:57:15 -0400

    Finalizacion del CRUD, incluyendo seeder

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,4 +4,24 @@
     <div class="row">
         <h1>Principal</h1>
     </div>
+    <div class="row">
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    @php
+                        $count = 0;
+                        foreach ($users as $user) {
+                            $count++;
+                        }
+                    @endphp
+                    <h3>{{$count}}</h3>
+                    <p>USUARIOS REGISTRADOS</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+                <a href="{{route('users.index')}}" class="small-box-footer">MAS INFORMACION<i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    </div>
 @endsection
